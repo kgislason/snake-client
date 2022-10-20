81bf9280cb7cc8dc282dcef9b7be50ad52f49b66
Kristy Gislason <kristygislason@gmail.com>
2022-10-19 20:48:35 -0700

    Added some messages to b n m kets

--- input.js
+++ input.js
@@ -14,16 +14,20 @@ const handleUserInput = (data) => {
   // Close the program if user input CTRL+C
   if (data === 'w') {
     conn.write("Move: up");
-  } else   if (data === 'a') {
+  } else if (data === 'a') {
     conn.write("Move: left");
-  } else   if (data === 's') {
+  } else if (data === 's') {
     conn.write("Move: down");
-  } else   if (data === 'd') {
+  } else if (data === 'd') {
     conn.write("Move: right");
   } else if (data === '\003') {
     process.exit();
-  } else {
-    console.log(data);
+  } else if (data === 'm') {
+    conn.write("Say: Hey!");
+  } else if (data === 'n') {
+    conn.write("Say: No you didn't!");
+  } else if (data === 'b') {
+    conn.write("Say: Bye!");
   }
 }
 
